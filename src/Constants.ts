b32cee64b8636491283c85e2043c4d5b4961d1e8
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2026-04-09 17:15:43 +0200

    Functional data form.

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 export const TOOL_INTERNAL_NAME = "lrc-workbench";
 export const TOOL_NAME = "LRC Workbench by Azaria";
 export const TOOL_FULL_NAME = `${TOOL_NAME} (lrc-workbench.azariadev.dev)]`;
-export const TOOL_VERSION = "0.2.2";
+export const TOOL_VERSION = "0.2.3";
